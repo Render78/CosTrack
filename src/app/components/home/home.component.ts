edd8621c291c0e704029f52ea1e885eeddceba11
Render78 <mariojuliantor@hotmail.com>
2025-12-07 23:09:39 -0300

    change welcome name and add modal component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { StatCardComponent } from '../stat-card/stat-card.component';
+import { ModalNewCosplayComponent } from "../modal-new-cosplay/modal-new-cosplay.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [StatCardComponent],
+  imports: [StatCardComponent, ModalNewCosplayComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
