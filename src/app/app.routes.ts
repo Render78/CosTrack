99345904b7eec106ee52c226dafe90b35c2af9fc
Render78 <mariojuliantor@hotmail.com>
2025-12-09 23:42:50 -0300

    add list cosplays route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TableListCosplayComponent } from './components/table-list-cosplay/table-list-cosplay.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'cosplay/list',
+        component: TableListCosplayComponent
+    }
+];
